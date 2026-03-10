dfc3786e97d7ad1203e84b79ab05f6ef1b028ba9
Sai Teja <mandigasaiteja150@gmail.com>
2026-03-11 01:12:47 +0530

    Updated settings for deployment

--- career_ai/settings.py
+++ career_ai/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
